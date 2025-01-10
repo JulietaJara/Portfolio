a8cb2390b1e84a9d25f9af842de8113bc66b88d8
Julieta <jarajuli13@gmail.com>
2025-01-10 04:11:39 -0300

    CV y estilos

--- mi-portfolio/app/components/Hero.tsx
+++ mi-portfolio/app/components/Hero.tsx
@@ -6,17 +6,25 @@ export default function Hero() {
     <section className="py-20 text-center bg-gradient-to-b from-lilac-100 to-lilac-200">
       <h1 className="text-4xl font-bold mb-4">
         Hola, soy <span style={{ color: 'rgb(110, 1, 166)' }}>Julieta Jara
-        <span role="img" aria-label="emoji" className="ml-2">💜</span> 
+          <span role="img" aria-label="emoji" className="ml-2">💜</span>
         </span>
       </h1>
 
       <p className="text-xl mb-8 text-lilac-700">Desarrolladora Junior</p>
 
+      <Link href="https://drive.google.com/file/d/1WApFrVq0NjJh4AvYhnQen7u49yYFyQvm/view?usp=sharing" passHref>
+        <Button className="bg-lilac-600 hover:bg-lilac-800 text-white mr-2">
+          Mi CV
+          <span role="img" aria-label="emoji" className="ml-2">🙂</span>
+        </Button>
+      </Link>
+
       <Link href="#contact" passHref>
         <Button className="bg-lilac-600 hover:bg-lilac-800 text-white">
           Contáctame
+          <span role="img" aria-label="emoji" className="ml-2">📩</span>
         </Button>
       </Link>
     </section>
   );
-}
+}  
--- mi-portfolio/app/components/ui/button.tsx
+++ mi-portfolio/app/components/ui/button.tsx
@@ -5,23 +5,20 @@ interface ButtonProps {
     className?: string;
     asChild?: boolean;
     variant?: 'primary' | 'secondary' | 'ghost';  
-  }
-  
-  export default function Button({ children, className, asChild, variant = 'primary' }: ButtonProps) {
+}
+
+export default function Button({ children, className, asChild, variant = 'primary' }: ButtonProps) {
     const Component = asChild ? 'a' : 'button';
-  
+
     const variantClass = variant === 'primary' 
-      ? 'bg-lilac-600 hover:bg-lilac-700 text-white' 
-      : variant === 'secondary'
-      ? 'bg-lilac-100 text-lilac-700'
-      : 'bg-transparent text-lilac-700 border border-lilac-700'; 
-  
+        ? 'bg-lilac-600 hover:bg-lilac-700 text-white' 
+        : variant === 'secondary'
+        ? 'bg-lilac-100 text-lilac-700'
+        : 'bg-transparent text-lilac-700 border border-lilac-700'; 
+
     return (
-      <Component className={`px-4 py-2 rounded-md font-semibold ${variantClass} ${className}`}>
-        {children}
-      </Component>
+        <Component className={`px-4 py-2 rounded-full font-semibold ${variantClass} ${className}`}>
+            {children}
+        </Component>
     );
-  }
-  
-
-
+}
